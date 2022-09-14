227882cab3b33a8c90a8036df08231b48eb61cb3
Rami <rami@jwcorp.fr>
2022-09-14 20:06:05 +0200

    .

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ export default function Menu() {
     return (
         <div className="menu">
             <ul>
-                <li><NavLink to="/" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>Home</NavLink></li>
+                <li><NavLink to="/" className={({isActive}) => (isActive ? "activeLink" : undefined)}>Home</NavLink></li>
                 <li><NavLink to="/add" className={({isActive}) => (isActive ? "activeLink" : undefined)} >Add Technos</NavLink></li>
                 <li><NavLink to="/list">All Technos</NavLink></li>
             </ul>
