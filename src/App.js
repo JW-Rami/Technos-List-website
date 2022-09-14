cc203f765b13c604dd517f4cbc3cb6e322e8ab10
Rami <rami@jwcorp.fr>
2022-09-14 16:13:02 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-// import {Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './css/application.css';
 import Home from './pages/Home.js'
 import Menu from './components/Menu.js'
 import TechnoAdd from './pages/TechnoAdd';
 import TechnoList from './pages/TechnoList';
-// Pas besoin d'import avec les accolade car on export par défaut
+// Pas besoin d'import avec {} car on export par défaut
 
 function App() {
   return (
     <>
-    {/* <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/add" element={<TechnoAdd/>}/>
+    <Routes>
+      <Route path="/" element={<Home /> } />         {/* On indique ici que Home est à la racine */}
+      <Route path="/add" element={<TechnoAdd />} />
+      <Route path="/list" element={<TechnoList/>} />
     
-    </Routes> */}
-    <Menu />
+    </Routes>
+    {/* <Menu />
     <Home />
     <TechnoAdd/>
-    <TechnoList/>
+    <TechnoList/> */}
     </>
     
   );
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// Import du Browser Router entre {} car pas mis d'export Default
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <BrowserRouter>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
