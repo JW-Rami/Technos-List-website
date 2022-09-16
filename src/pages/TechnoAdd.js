63ae763d38e4dc70da1ab1ce4b4b131530a6ec9a
JW-Rami <rami@jwcorp.fr>
2022-09-16 21:26:36 +0200

    .

--- src/pages/TechnoAdd.js
+++ src/pages/TechnoAdd.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function TechnoAdd(props) {
   const [ techno, setTechno ] = useState({
-    technoname: 'lmkmlklk',
+    technoname: '',
     technocategory: '',
     technodescription: ''
   }); // ajouter ici les autres éléments qu'on veut afficher pour l'evenement submit.
@@ -13,6 +13,10 @@ const { handleAddTechno } = props;
     e.preventDefault();
     handleAddTechno(techno);
   }
+  function handleChange(e) {
+    const {name, value} = e.target;                    // sert à récupérer le nom et la valeur des informations du formulaires
+    setTechno({...techno, [name]: value });            // permet de cloner l'objet techno avec le spread operator: principe d'immutabilité
+  }
   return (
     <div className="techno-add">
       <h1>Add a techno</h1>
@@ -22,18 +26,18 @@ const { handleAddTechno } = props;
           {/* Pour que l'evenemnt soit passé dans la fonction */}
           <label htmlFor="technoname">Name :</label>
           <br />
-          <input type="text" name="technoname" id="technoname" value={techno.technoname} />
+          <input type="text" name="technoname" id="technoname" value={techno.technoname} onChange={(e) => handleChange(e)} />
           <br />
           <label htmlFor="techno-category">Category :</label>{" "}
           {/*"techno-category pour qu'il soit relié à la catégorie de techno*/}
           <br />
-          <select name="techno-category" id="techno-category">
+          <select name="techno-category" id="techno-category" value={techno.technocategory} onChange={(e) => handleChange(e)}>
             <option value="">Select a category</option>
             <option value="front">Front</option>
             <option value="back">Back</option>
             <option value="fullstack">Full Stack</option>
             <option value="other">Other</option>
-          </select>{" "}
+          </select>
           <br />
           <label htmlFor="techno-description">Description :</label>
           <br />
