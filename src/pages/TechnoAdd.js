15b1997ddb909311cec9ff34c2354a6aff7577db
JW-Rami <rami@jwcorp.fr>
2022-09-16 16:56:24 +0200

    .

--- src/pages/TechnoAdd.js
+++ src/pages/TechnoAdd.js
@@ -1,20 +1,25 @@
+import { useState } from "react";
+
 export default function TechnoAdd(props) {
- const { handleAddTechno} = props;
- const techno = { // on recupere pas les données de l'utilisateur on verifie dabord que ça fonctionne
-  name: 'React',
-  category:'front',
-  description: 'Learn React'
- };
- 
- function handleSubmit(e) {
-  e.preventDefault();
-  handleAddTechno(techno);
- }
+  const { handleAddTechno } = props; // ajouter ici les autres éléments qu'on veut afficher pour l'evenement submit.
+  const techno = {
+    // on recupere pas les données de l'utilisateur on verifie dabord que ça fonctionne
+    name: "React",
+    category: "front",
+    description: "Learn React",
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    handleAddTechno(techno);
+  }
   return (
     <div className="techno-add">
       <h1>Add a techno</h1>
       <div>
-        <form onSubmit={(e) => handleSubmit(e)}> {/* Pour que l'evenemnt soit passé dans la fonction */}
+        <form onSubmit={(e) => handleSubmit(e)}>
+          {" "}
+          {/* Pour que l'evenemnt soit passé dans la fonction */}
           <label htmlFor="techno-name">Name :</label>
           <br />
           <input type="text" name="techno-name" id="techno-name" />
@@ -28,7 +33,8 @@ export default function TechnoAdd(props) {
             <option value="back">Back</option>
             <option value="fullstack">Full Stack</option>
             <option value="other">Other</option>
-          </select> <br />
+          </select>{" "}
+          <br />
           <label htmlFor="techno-description">Description :</label>
           <br />
           <textarea
@@ -36,7 +42,8 @@ export default function TechnoAdd(props) {
             id="techno-description"
             cols="30"
             rows="10"
-          ></textarea> <br />
+          ></textarea>{" "}
+          <br />
           <input type="submit" value="Add Techno" className="btn" />
         </form>
       </div>
